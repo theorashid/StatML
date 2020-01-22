1814addbbe72b3f96fc691cf8d87fd69b233b253
Theo Rashid <theoaorashid@gmail.com>
2020-01-22 11:56:26 +0000

    End of tutorial

---
+++ Lab 2/LDA.R
@@ -0,0 +1,43 @@
+# Theo AO Rashid -- January 2020
+
+# ----- Machine Learning: Lab 2 -----
+library(MASS)
+set.seed(1)
+
+# Iris dataset in the form:
+# Sepal.Length | Sepal.Width | Petal.Length | Petal.Width | Species
+iris.data=iris
+
+# Classification task: Classify if flower is virginica (TRUE) or not (FALSE)
+iris.data$Species <- iris.data$Species == "virginica"
+
+# Pairs produces a matrix of scatterplots
+# pairs(iris[,1:4],col=iris$Species) # plot for all species
+# pairs(iris.data[,1:4],col=iris.data$Species+1) # plot for virginica or not
+
+# Divide the data into test and training (here 60% train, 40% test)
+training_sample <- sample(c(TRUE, FALSE), nrow(iris.data), replace = T, prob = c(0.6,0.4))
+train <- iris.data[training_sample, ]
+test <- iris.data[!training_sample, ]
+
+# Linear Discriminant Analysis (LDA)
+
+# Fit LDA to training set
+# lda function from MASS package
+# "Species vs everything else, please fit the training data"
+iris.lda <- lda(Species ~ ., train)
+
+# Evaluate on the training set
+lda.train <- predict(iris.lda)
+train$lda <- lda.train$class
+# table(train$lda,train$Species) # confusion matrix (predicted, columns)
+# The model fits the training set fairly well (sum of diagonals correctly predicted)
+
+# Generate the posterior probabilities (p(y=1|data) in the 2nd column)
+train$lda_proba <- predict(iris.lda)$posterior[,2]
+
+# Evaluate on the test set
+lda.test <- predict(iris.lda, test)
+test$lda <- lda.test$class
+# table(test$lda,test$Species) # confusion matrix for test set
+test$lda_proba <- predict(iris.lda, test)$posterior[,2]
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Lab sessions from the Machine Learning course by Dr Sarah Filippi at Imperial Co
 
 Lab 1 -- Supervised learning with linear regression models
 Investigate the effect that increasing model complexity has on the mean squared error (MSE), and consider the use of cross validation as a means of choosing the most appropriate predictive model, and examine the impact of regularisation techniques.
+
+Lab 2 -- Perform classification via Linear Discriminant Analysis and logistic regression
