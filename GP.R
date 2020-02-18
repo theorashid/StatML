e411a7f5023a38dd72da88179fb30cc89d21e65a
Theo Rashid <theoaorashid@gmail.com>
2020-02-18 12:55:20 +0000

    calculated posteriors

--- GP.R
+++ GP.R
@@ -15,15 +15,15 @@ set.seed(1)
 # Typically they have amplitude parameter (sigma)
 # and length-scale (l) -- a smoothness parameter
 
-# # Constant
-# const_kernel <- function(x_i, x_j, C = 1) {
-#     return(C)
-# }
+# Constant
+const_kernel <- function(x_i, x_j, C = 1) {
+    return(C)
+}
 
-# # Linear
-# lin_kernel <- function(x_i, x_j) {
-#     return(t(x_i) * x_j)
-# }
+# Linear
+lin_kernel <- function(x_i, x_j) {
+    return(x_i * x_j)
+}
 
 # Squared exponential 
 se_kernel <- function(x_i, x_j, sigma = 1, l = 1) {
@@ -53,16 +53,16 @@ matern_kernel <- function(x_i, x_j, nu = 1.5, sigma = 1, l = 1) {
 
 # ----- CALCULATE THE COVARIANCE MATRIX -----
 # generate covariance matrix for points in `x` using given kernel function
-cov_matrix <- function(x, kernel_fn, ...) {
-    return(outer(x, x, function(a, b) kernel_fn(a, b, ...)))
+cov_matrix <- function(x1, x2, kernel_fn, ...) {
+    return(outer(x1, x2, function(a, b) kernel_fn(a, b, ...)))
 }
 
 # Plot the covariance matrix in 2D
 x <- seq(0, 5, length.out = 501)  # x-coordinates between 0 and 5
-# image(x = x, y = x, z = cov_matrix(x, matern_kernel))
+# image(x = x, y = x, z = cov_matrix(x, x, matern_kernel))
 
 # Plot the covariance matrix in 1D
-# data <- cov_matrix(x, se_kernel) 
+# data <- cov_matrix(x, x, se_kernel) 
 # data <- data[sample(1:length(x), 1),] # Pick a random row
 # plot_cov <- ggplot(data.frame(x, data), aes(x = x, y = data)) +
 #     geom_line(size = 0.5) +
@@ -70,35 +70,36 @@ x <- seq(0, 5, length.out = 501)  # x-coordinates between 0 and 5
 #     theme_minimal()
 # print(plot_cov)
 
-# 2. Draw samples from the Gaussian process GP(0, K)
-draw_GP_samples <- function(x, N, kernel_fn, ...) {
-    # x -- coordinates
-    # N -- number of draws
-    Y <- matrix(NA, nrow = length(x), ncol = N)
-    for (i in 1:N) {
-        K <- cov_matrix(x, kernel_fn, ...)
-        # use mvnorm from MASS with zero vector mean, one sample at a time
-        Y[, i] <- mvrnorm(n = 1, mu = rep(0, times = length(x)), Sigma = K)
-    }
-    return(Y) # each column is a sample
+# 2. Draw sample from the Gaussian process GP(m(.), K(.,.))
+GP_sample <- function(m, K) {
+    # m -- mean function vector
+    # K -- covariance matrix
+    # use mvnorm from MASS
+    GP_samp <- mvrnorm(n = 1, mu = m, Sigma = K) # one sample
+    return(GP_samp) # each column is a sample
 }
 
+# Plot 3 samples, vary l
 N_samples <- 3
-Y <- draw_GP_samples(x, N = N_samples, kernel_fn = period_kernel, l = 10)
-
-# # Plotting works only for N_samples = 3
-# plot_sample <- ggplot(data.frame(x, Y), aes(x = x)) +
-#     geom_line(aes(y = Y[,1])) +
-#     geom_line(aes(y = Y[,2])) +
-#     geom_line(aes(y = Y[,3])) +
-#     labs(title = "", x = "", y = "") +
-#     theme_minimal()
-# print(plot_sample)
+Y <- matrix(NA, nrow = length(x), ncol = N_samples)
+K <- cov_matrix(x, x, matern_kernel, l = 10)
+for (i in 1:N_samples) {
+    # zero mean vector
+    Y[, i] <- GP_sample(m = matrix(0, length(x), 1), K = K)
+}
+# Plotting works only for N_samples = 3
+plot_sample <- ggplot(data.frame(x, Y), aes(x = x)) +
+    geom_line(aes(y = Y[,1])) +
+    geom_line(aes(y = Y[,2])) +
+    geom_line(aes(y = Y[,3])) +
+    labs(title = "", x = "", y = "") +
+    theme_minimal()
+print(plot_sample)
 # Varying length scale works as expected
 # Shown best for periodic kernel function
 
 # 3. Add data points
-# Let's try for population data
+# ----- ONS POPULATION DATA -----
 pop_df <- read.csv("/Users/tar15/Dropbox/SPH/Data/Unit Test/pop_IMD_2004_17_hf.csv", row.names = 1)
 # Add the populations over age groups and sexes
 pop_LSOA_df <- pop_df %>%
@@ -117,24 +118,44 @@ y_train <- pop_samp_df$population
 kernel <- matern_kernel # choose kernel
 sigma_noise <- 0 # noise model
 
+# Test data -- around the range of years
+X_test <- seq(min(X_train) - 5, max(X_train) + 5, length.out = 47)
+
 # ----- PRIORS -----
 # prior is a sample from GP(0, K(., .))
 # We have no knowledge to select other mean function value
-prior_f <- draw_GP_samples(X_train, N = 1, kernel_fn = kernel)
-# plot_prior <- ggplot(data.frame(X_train, prior_f), aes(x = X_train, y = prior_f)) +
-#     geom_line() +
+m_prior = matrix(0, length(X_test), 1) # prior mean = m(X_*) = 0
+k_prior <- cov_matrix(X_test, X_test, kernel) # prior variance k(X_*, X_*)
+
+prior_f <- GP_sample(m = m_prior, K = k_prior) # example draw
+# plot_prior <- ggplot() +
+#     geom_point(aes(x = X_train, y = y_train)) +
+#     geom_line(aes(x = X_test, y = prior_f)) +
 #     labs(title = "", x = "", y = "") +
 #     theme_minimal()
 # print(plot_prior)
 
-K_ss <- kernel(X_train, X_train) # prior variance
-
 # ----- POSTERIOR -----
-# Test data
-X_test <- seq(min(X_train) - 10, max(X_train) + 10, length.out = 10)
-K <- kernel(X_test, X_test)
-# m_post <- m_prior + solve(kernel(X_train, X_test) + sigma_noise^2 * diag(length(X_train))) %*% (y_train - m_prior(X_train))
+# X -- X_train; X_* -- X_test
+K <- cov_matrix(X_train, X_train, kernel) # K = k(X, X)
 
-# Posterior predictive distribution at test inputs X_s
+# posterior mean
+kalman_gain <- cov_matrix(X_test, X_train, kernel) %*% solve((K + sigma_noise^2 * diag(length(X_train))))
+m_post <- m_prior + kalman_gain %*% (y_train - matrix(0, length(X_train), 1))
+
+# posterior variance
+k_post <- k_prior - kalman_gain %*% cov_matrix(X_train, X_test, kernel)
+
+post_f <- GP_sample(m = m_post, K = k_post) # example draw
+# plot_post <- ggplot() +
+#     geom_point(aes(x = X_train, y = y_train)) +
+#     geom_line(aes(x = X_test, y = post_f)) +
+#     labs(title = "", x = "", y = "") +
+#     theme_minimal()
+# print(plot_post)
+
+# Posterior predictive distribution at test inputs X_*
 # Obtained by Gaussian conditioning
 
+# WORK OUT MARGINAL LIKELIHOOD OF THE DATA
+# AND OPTIMISE USING optim
