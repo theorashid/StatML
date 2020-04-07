139d90a0f4608bc93851fccab882acc10dc59c4e
Theo Rashid <theoaorashid@gmail.com>
2020-04-07 16:33:53 +0100

    demo for age vs death

--- GP_demo.R
+++ GP_demo.R
@@ -7,7 +7,7 @@ library(MASS)
 library(ggplot2)
 library(dplyr)
 
-#set.seed(1)
+set.seed(1)
 
 # ----- DEFINE SOME KERNELS -----
 # Squared exponential 
@@ -36,41 +36,43 @@ cov_matrix <- function(x1, x2, kernel_fn, ...) {
     return(outer(x1, x2, function(a, b) kernel_fn(a, b, ...)))
 }
 
-# ----- ONS POPULATION DATA -----
+# ----- SIMULATED DEATH DATA -----
 # Simulated deaths in Hammersmith and Fulham 2004-2017
 mortality <- read.csv("/Users/tar15/Dropbox/SPH/Data/Unit Test/mortsim_hf.csv", row.names = 1)
 mortality_m <- mortality %>%
     filter(sex == 1) %>%
     select(-sex) %>%
-    filter(LSOA2011 == "E01001851") %>% # focus on one LSOA
-    select(YEAR, deaths) %>% # focus only on year, deaths and populations
-    group_by(YEAR) %>%
-    summarise(deaths = sum(deaths))
+    select(age_group, age_group.id, deaths, population) %>% # focus only on age, deaths and populations
+    group_by(age_group.id) %>%
+    summarise(death_rates = sum(deaths)/sum(population)) # emperical death rates
+mortality_m$age = c(0.5, 3, 7.5, 12.5, 17.5, 22.5, 27.5, 32.5, 37.5, 42.5, 47.5, 52.5, 57.5,
+                    62.5, 67.5, 72.5, 77.5, 82.5, 87.5) # bin midpoints (although last is 85+)
+mortality_m <- mortality_m[c(3,2)] # age and death rates only
 
-plot_year_deaths <- ggplot(mortality_m, aes(x = YEAR, y = deaths)) +
+plot_year_deaths <- ggplot(mortality_m, aes(x = age, y = death_rates)) +
     geom_point()
 
-X_train <- mortality_m$YEAR
-y_train <- scale(mortality_m$deaths) # Rescale the data so m = 0 is appropriate
+X_train <- mortality_m$age
+y_train <- mortality_m$death_rates
 
-X_test <- seq(min(X_train) - 5, max(X_train) + 5, length.out = 576) # data around the years
+X_test <- seq(0, 100, length.out = 501) # data around from age 0 to 100
 
 # ----- GP inference -----
 # Hyperparameters
-kernel <- matern_kernel # choose kernel
-sigma_noise <- 0.1 # noise model
-length_scale <- 1 # for SE/Matern kernel
-amplitude <- 1 # for SE/Matern kernel
+kernel <- se_kernel # choose kernel
+sigma_noise <- 0.01 # noise model
+length_scale <- 30 # for SE/Matern kernel
+amplitude <- 0.1 # for SE/Matern kernel
 
 # Prior -- sample from GP(0, K(., .))
-m_prior = matrix(0, length(X_test), 1)
+m_prior = matrix(0, length(X_test), 1) # flat prior choice
 k_prior <- cov_matrix(X_test, X_test, kernel, sigma = amplitude, l = length_scale)
 
 prior_f <- mvrnorm(n = 1, mu = m_prior, Sigma = k_prior) # example draw from prior
 plot_prior <- ggplot() +
     geom_point(aes(x = X_train, y = y_train)) +
     geom_line(aes(x = X_test, y = prior_f)) +
-    labs(title = "", x = "", y = "") +
+    labs(title = "", x = "age", y = "death rate") +
     theme_minimal()
 
 # Posterior
@@ -89,5 +91,5 @@ plot_post <- ggplot() +
     geom_point(aes(x = X_train, y = y_train)) +
     geom_line(aes(x = X_test, y = post_f)) +
     geom_ribbon(aes(x=X_test, ymin=post_f-ci_95, ymax=post_f+ci_95),fill="blue", alpha=0.2) +
-    labs(title = "", x = "", y = "") +
+    labs(title = "", x = "age", y = "death rate") +
     theme_minimal()
